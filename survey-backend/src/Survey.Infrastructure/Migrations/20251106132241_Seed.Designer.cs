70be0f4a329839f339df6fc441073eb13d9da0cd
MohamedBenMassouda <benmassoudamohamed1@gmail.com>
2025-11-06 14:23:22 +0100

    Added admin user

--- survey-backend/src/Survey.Infrastructure/Migrations/20251106132241_Seed.Designer.cs
+++ survey-backend/src/Survey.Infrastructure/Migrations/20251106132241_Seed.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Survey.Infrastructure.Context;
@@ -11,9 +12,11 @@ using Survey.Infrastructure.Context;
 namespace Survey.Infrastructure.Migrations
 {
     [DbContext(typeof(SurveyDbContext))]
-    partial class SurveyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251106132241_Seed")]
+    partial class Seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ survey-backend/src/Survey.Infrastructure/Migrations/20251106132241_Seed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Survey.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "admins",
+                columns: new[] { "Id", "CreatedAt", "Email", "FullName", "IsActive", "LastModified", "PasswordHash", "UpdatedAt" },
+                values: new object[] { new Guid("a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d"), new DateTime(2025, 11, 6, 13, 13, 7, 709, DateTimeKind.Utc), "admin@example.com", "Admin User", true, null, "$2a$11$aMb7VlbBT6D9UpfZCJUPHOjfoqqtbI2wB70.d/zK.obEPbaQo7GDW", new DateTime(2025, 11, 6, 13, 13, 7, 709, DateTimeKind.Utc) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "admins",
+                keyColumn: "Id",
+                keyValue: new Guid("a1b2c3d4-e5f6-4a5b-8c9d-0e1f2a3b4c5d"));
+        }
+    }
+}
